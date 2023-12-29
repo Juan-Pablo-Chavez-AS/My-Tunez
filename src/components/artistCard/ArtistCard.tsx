461e0f37955310656d9734a90fd19c1b1a74e48c
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-12-29 11:29:28 -0400

    Artist list now has an all artist option

--- src/components/artistCard/ArtistCard.tsx
+++ src/components/artistCard/ArtistCard.tsx
@@ -3,14 +3,16 @@ import { Artist } from "../../types/types";
 import { Stack, Typography } from "@mui/material";
 
 interface ArtistCardProps {
-  artist: Artist
-  setArtist: (artist: Artist) => void
+  artist: Artist | null;
+  setArtist: (artist: Artist | null) => void
 }
+
+const defaultImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png"
 
 export const ArtistCard:React.FC<ArtistCardProps> = ({ artist, setArtist }) => {
   const imageSize = 40;
   return <Stack border={1} borderColor={"red"} direction={"row"} alignItems={"center"} p={1} spacing={0.5} onClick={() => setArtist(artist)}>
-    <img src={artist.image} alt={artist.name} width={imageSize} height={imageSize}/>
-    <Typography flexGrow={1} p={1} textAlign={"center"}>{artist.name}</Typography>
+    <img src={artist?.image || defaultImage} alt={artist?.name} width={imageSize} height={imageSize}/>
+    <Typography flexGrow={1} p={1} textAlign={"center"}>{artist?.name || "All artist"}</Typography>
   </Stack>;
 }
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 import ArtistRepository from "../../storage/artist.repository";
 
 interface SidebarProps {
-  setArtist: (artist: Artist) => void
+  setArtist: (artist: Artist | null) => void
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({ setArtist }) => {
@@ -18,6 +18,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ setArtist }) => {
 
   return (
     <Stack direction={"column"} p={1} spacing={2}>
+      <ArtistCard setArtist={setArtist} artist={null}/>
       {artists.map((artist: Artist, index: number) => {
         return <ArtistCard setArtist={setArtist} artist={artist} key={index}/>
       })}
