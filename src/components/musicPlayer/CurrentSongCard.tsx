f7d195e1fe20afff3a42e25f617fa7546014ce8f
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2024-01-03 17:23:28 -0400

    Current song card added

---
+++ src/components/musicPlayer/CurrentSongCard.tsx
@@ -0,0 +1,48 @@
+import { Card, CardContent, CardMedia, Paper, Stack, Typography } from "@mui/material";
+import { useCurrentPlaylistStore } from "../../stores/CurrentPlaylistStore";
+import { useEffect, useState } from "react";
+import AlbumRepository from "../../storage/album.repository";
+import { Album } from "../../types/types";
+import ArtistRepository from "../../storage/artist.repository";
+
+
+export const CurrentSongCard = () => {
+  const { currentSong } = useCurrentPlaylistStore();
+  const [album, setCover] = useState<Album | undefined>(undefined);
+  const [artist, setArtist] = useState<string>("");
+
+  useEffect(() => {
+    if (currentSong) {
+      const albumRepository = new AlbumRepository();
+      const album = albumRepository.getById(currentSong.album);
+      setCover(album);
+      if (album) {
+        const artistRepository = new ArtistRepository();
+        const artist = artistRepository.getById(album.artist)?.name;
+        setArtist(artist || "");
+      }
+    }
+  }, [currentSong]);
+
+  return (<>
+    {
+      currentSong ?
+      <Stack component={Card} direction={"row"} height={"100%"} p={1} width={"20%"} >
+        <CardMedia
+          component="img"
+          sx={{ width: 100 }}
+          image={album?.image}
+          alt={currentSong?.title}
+        />
+        <CardContent component={Paper} sx={{ width: "100%", paddingLeft: 1, height: "100%" }} elevation={10} >
+          <Typography fontWeight={"bold"} fontSize={"1.2rem"}>{currentSong.title}</Typography>
+          <Typography fontSize={"1.1rem"}>{artist}</Typography>
+        </CardContent>
+      </Stack>
+      :
+      <>
+      </>
+    }
+  </>
+  )
+}
--- src/components/playerToolbar/PlayerToolbar.tsx
+++ src/components/playerToolbar/PlayerToolbar.tsx
@@ -1,12 +1,25 @@
 import { Stack, Typography } from "@mui/material"
 import { MusicPlayer } from "../musicPlayer/MusicPlayer"
+import { CurrentSongCard } from "../musicPlayer/CurrentSongCard"
 
 export const PlayerToolbar = () => {
   return (
-    <Stack direction="row" spacing={2} justifyContent={"space-around"} alignItems={'center'} height={'100%'} bgcolor={"#242424"}>
-      <Typography  borderColor={"black"}textAlign={"center"}>Player Info Settings</Typography>
+    <Stack
+      direction="row"
+      spacing={1}
+      justifyContent={"space-around"}
+      alignItems={'center'}
+      height={'100%'}
+      bgcolor={"#242424"}
+      sx={{
+        "& *": {
+          flexGrow: 1
+        }
+      }}
+    >
+      <CurrentSongCard />
       <MusicPlayer />
-      <Typography  borderColor={"black"} textAlign={"center"}>Search bar</Typography>
+      <Typography width={"20%"} display={"block"} borderColor={"black"} textAlign={"center"}>Search bar</Typography>
     </Stack>
   )
 }
